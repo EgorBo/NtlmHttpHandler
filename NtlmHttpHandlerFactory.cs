f6fc1828b8bdd21d7e119f351970f6e123f70f2e
EgorBo <egorbo@gmail.com>
2020-01-15 19:16:01 +0300

    Add Linker tips to exception messages

--- NtlmHttpHandlerFactory.cs
+++ NtlmHttpHandlerFactory.cs
@@ -6,6 +6,17 @@ namespace NtlmHttpHandler
 {
     public class NtlmHttpHandlerFactory
     {
+        public const string LinkerTipText = @"
+
+Make sure you you've added the following types to your linker.xml:
+
+<linker>
+    <assembly fullname=""System.Net.Http"">
+        <type fullname=""System.Net.Http.HttpClientHandler*"" />
+        <type fullname=""System.Net.Http.MonoWebRequestHandler*"" />
+    </assembly>
+</linker>";
+
         public static HttpClientHandler Create()
         {
             Type androidenvType = Type.GetType("Android.Runtime.AndroidEnvironment, Mono.Android");
@@ -18,13 +29,13 @@ namespace NtlmHttpHandler
 
             Type monoHandlerType = Type.GetType("System.Net.Http.MonoWebRequestHandler, System.Net.Http");
             if (monoHandlerType == null)
-                throw new InvalidOperationException("`System.Net.Http.MonoWebRequestHandler` was not found in System.Net.Http.");
+                throw new InvalidOperationException("`System.Net.Http.MonoWebRequestHandler` was not found in System.Net.Http." + LinkerTipText);
 
             ConstructorInfo[] internalMonoHandlerCtors = monoHandlerType.GetConstructors(
                 BindingFlags.NonPublic | BindingFlags.Instance);
 
             if (internalMonoHandlerCtors.Length < 1)
-                throw new InvalidOperationException("Internal parameter-less constructor for `System.Net.Http.MonoWebRequestHandler` was not found.");
+                throw new InvalidOperationException("Internal parameter-less constructor for `System.Net.Http.MonoWebRequestHandler` was not found." + LinkerTipText);
 
             object internalMonoHandler = internalMonoHandlerCtors[0].Invoke(null);
 
@@ -32,7 +43,7 @@ namespace NtlmHttpHandler
                 typeof(HttpClientHandler).GetConstructors(BindingFlags.NonPublic | BindingFlags.Instance);
 
             if (httpClientHandlerCtors.Length < 1)
-                throw new InvalidOperationException("`internal HttpClientHandler(IMonoHttpClientHandler)` constructor was not found in `System.Net.Http.HttpClientHandler`.");
+                throw new InvalidOperationException("`internal HttpClientHandler(IMonoHttpClientHandler)` constructor was not found in `System.Net.Http.HttpClientHandler`." + LinkerTipText);
 
             return (HttpClientHandler)httpClientHandlerCtors[0].Invoke(new [] { internalMonoHandler });
         }
